d8f2194a04614703dd09fbce105cb83290a389ca
ovallner <ovallner@gmail.com>
2016-02-23 17:53:58 -0600

    Implemented readData()

--- sortingcompetition.cpp
+++ sortingcompetition.cpp
@@ -1,7 +1,34 @@
 #include "sortingcompetition.h"
+#include <vector>
+
+using namespace std;
 
 SortingCompetition::SortingCompetition()
 {
 
 }
 
+SortingCompetition::SortingCompetition(const string &inputFileName)
+{
+    setFileName(inputFileName);
+}
+
+void SortingCompetition::setFileName(const string &inputFileName)
+{
+
+}
+
+bool SortingCompetition::readData()
+{
+    if(inFile.fail())
+        return false;
+
+    tokens.clear();
+    string word;
+    while(!(inFile.eof()))
+    {
+        inFile >> word;
+        tokens.push_back(word);
+    }
+    return true;
+}
