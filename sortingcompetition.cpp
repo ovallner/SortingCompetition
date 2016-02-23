ff4e395d4891b6e3c6a96c2f78da21f56cb2783d
Kellen Schmidt <kellenschmidt31@gmail.com>
2016-02-23 17:32:13 -0600

    Wrote setFileName() and add std:: for strings in header

--- sortingcompetition.cpp
+++ sortingcompetition.cpp
@@ -1,7 +1,24 @@
 #include "sortingcompetition.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
 
 SortingCompetition::SortingCompetition()
 {
 
+}
+
+void SortingCompetition::setFileName(const string& inputFileName)
+{
+    inFile.open(inputFileName);
+
+    // Check if input file opened correctly
+    if(!inFile)
+    {
+        cerr << "Input file \"" << inputFileName << "\" not opened correctly\n";
+        exit(EXIT_FAILURE);
+    }
 }
 
